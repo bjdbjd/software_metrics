b88fdc222eff1996f2f6f9b3260125e71602e35c
ZYR <2262197831@qq.com>
2022-04-23 23:39:17 +0800

    新增用例图文件和用例图接口文件

--- app/api/use_case_diag.py
+++ app/api/use_case_diag.py
@@ -1,11 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_uploads import UploadSet, DOCUMENTS
+from ..core import use_case_diag
+
 from . import get_data, handle_error, params_check
 from ..util import trueReturn, falseReturn
 
 use_case_diag_blueprint = Blueprint('use_case_diag_blueprint', __name__, url_prefix='/use_case_diag')
 
+#usecase是UploadSet的名字，与配置文件中相同
+usecase = UploadSet('usecase', DOCUMENTS)
 
 @use_case_diag_blueprint.before_request
 @handle_error
 def before_request():
     get_data()
+
+@use_case_diag_blueprint.route('/', methods = ['POST'])
+def handle():
+    context = {}
+    file_url = None
+    print("aaa", request.files)
+    # usecase是html中input的name对应的值
+    if request.method == 'POST' and 'usecase' in request.files:
+        # 保存文件到本地
+        file_name = usecase.save(request.files['dataflow'])
+        # 返回文件路径
+        file_url = usecase.url(file_name)
+        basename = usecase.get_basename(file_name)
+        path = usecase.path(file_name)
+        print('file_url = ', file_url)
+        print('basename = ', basename)
+        print('path = ', path)
+    context[file_url] = file_url
+    return use_case_diag.getActorandUseCase(path)
+
+@use_case_diag_blueprint.route('/trial', methods = ['GET'])
+def trial():
+    return {'code': 200, 'msg': "Use Case Hello :)"}
--- app/core/use_case_diag.py
+++ app/core/use_case_diag.py
@@ -1,1 +1,38 @@
+import os
 
+def file_exists(filename):
+    #判断文件是否存在
+    if(filename==None):
+        return False
+    if(os.path.exists(filename) is False):
+        print('文件不存在！')
+        return False
+    return True
+
+def findStr(file_name, str):
+    with open(file_name, 'r', encoding="utf8") as file:
+        count = 0
+
+        #对每行数据进行查找
+        for line in file.readlines():
+            time = line.count(str)
+            count += time
+        return count
+
+def getActorandUseCase(file_name):
+    # 判断文件存在
+    if (file_exists(file_name) is False):
+        return {'code':322, 'msg': "Input File Error!"}
+
+    #查找执行者
+    actor_count = findStr(file_name, "<o:Actor Id")
+    #查找用例
+    use_case_count = findStr(file_name, "<o:UseCase Id")
+
+    print('Actor count: ', actor_count)
+    print('Use case count: ', use_case_count)
+    return{'code':200, 'actor': actor_count, 'use_case': use_case_count}
+
+if __name__ == '__main__':
+    file_name = "D:\diagram\CCMS.oom"
+    getActorandUseCase(file_name)
