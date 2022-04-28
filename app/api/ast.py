32968d7b14889d4693dc5e5f490ae19b5f5feeaa
johncage <573697439@qq.com>
2022-04-28 15:32:02 +0800

    FEA: 语法树解析

--- app/api/ast.py
+++ app/api/ast.py
@@ -1,6 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from werkzeug.datastructures import FileStorage
 from . import get_data, handle_error, params_check
+from ..core.ast import analyze
 from ..util import trueReturn, falseReturn
+import traceback
 
 ast_blueprint = Blueprint('ast_blueprint', __name__, url_prefix='/ast')
 
@@ -9,3 +12,15 @@ ast_blueprint = Blueprint('ast_blueprint', __name__, url_prefix='/ast')
 @handle_error
 def before_request():
     get_data()
+
+
+@ast_blueprint.route('/metric', methods=['POST'])
+def metric():
+    file: FileStorage = request.files.get('file')
+    content = file.stream.readlines()
+    try:
+        result = analyze(content)
+        return trueReturn(data=result)
+    except Exception:
+        traceback.print_exc()
+        return falseReturn(msg='系统错误')
